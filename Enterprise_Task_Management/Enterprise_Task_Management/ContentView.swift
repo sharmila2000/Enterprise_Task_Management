77729cec20a192a6164704b9f7ea4454988bc0fe
Ganesan, Sharmila - Contractor {PEP} <Sharmila.Ganesan.Contractor+pepsico@pepsico.com>
2026-07-04 22:55:49 +0530

    initial setup is done

---
+++ Enterprise_Task_Management/Enterprise_Task_Management/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Enterprise_Task_Management
+//
+//  Created by Sharmila Ganesan on 04/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
